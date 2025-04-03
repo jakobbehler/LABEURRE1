cde4c3ee9a437f2b21056d1cfec56ae44c1b6350
jakobbehler <jakob.behler@epfl.ch>
2025-04-03 15:37:01 +0200

    knobs with bottom labels

--- Source/knobSection.h
+++ Source/knobSection.h
@@ -16,7 +16,6 @@
 /*
 */
 
-
 class OtherLookAndFeel : public juce::LookAndFeel_V4
 {
 public:
@@ -27,7 +26,7 @@ public:
                           juce::Slider& slider) override;
 };
 
-
+//==============================================================================
 class CustomKnobComponent : public juce::Component
 {
 public:
@@ -38,73 +37,48 @@ public:
     void resized() override;
 
     void attach(juce::AudioProcessorValueTreeState& apvts, const juce::String& paramID);
+
     juce::Slider slider;
 
 protected:
-    
-    OtherLookAndFeel otherLookAndFeel;
-
     juce::String name;
-    
-    
+    OtherLookAndFeel otherLookAndFeel;
     bool shouldSnapToLabels = false;
-
-    
     std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> attachment;
 };
 
-// ----------------------------------------------------------------------
-
-class CompressionKnob : public CustomKnobComponent
+//==============================================================================
+class KnobWithLabel : public juce::Component
 {
 public:
-    CompressionKnob();
-    double snapValue(double value, juce::Slider::DragMode);
-protected:
-    std::vector<std::pair<double, juce::String>> labels;
+    KnobWithLabel(const juce::String& labelText);
+
+    void resized() override;
+
+    CustomKnobComponent& getKnob();
+
+private:
+    juce::Label label;
+    CustomKnobComponent knob;
 };
 
-
-
-class SaturationKnob : public CustomKnobComponent
-{
-public:
-    SaturationKnob();
-    double snapValue(double value, juce::Slider::DragMode);
-protected:
-    std::vector<std::pair<double, juce::String>> labels;
-};
-
-
-class HicutKnob : public CustomKnobComponent
-{
-public:
-    HicutKnob();
-};
-
-
-
-// ----------------------------------------------------------------------
-class knobSection  : public juce::Component
+//==============================================================================
+class knobSection : public juce::Component
 {
 public:
     knobSection();
     ~knobSection() override;
 
-    void paint (juce::Graphics&) override;
+    void paint(juce::Graphics&) override;
     void resized() override;
 
 private:
-    CompressionKnob compressorKnob;
-    SaturationKnob distortionKnob;
-    HicutKnob highCutKnob;
-    
-    OtherLookAndFeel otherLookAndFeel;
-    
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (knobSection)
-    
-};
+    KnobWithLabel compressorKnob;
+    KnobWithLabel distortionKnob;
+    KnobWithLabel highCutKnob;
 
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(knobSection)
+};
 
 // ------
 
